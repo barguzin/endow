5be8f794bfd7fc9ef8fcd4670916a4555ab01870
barguzin <noi@ucsb.edu>
2023-06-28 10:37:50 -0700

    add function to utils

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,3 @@
-# write a function to create an sf feature collection
 #' Make Point
 #'
 #' @param idvar (id variable associated with a geographic point)
@@ -17,4 +16,39 @@ make_point <- function(idvar, lon, lat) {
   geom = sf::st_sfc(sf::st_point(c(lon, lat)))
   my_sf = sf::st_sf(id_var = idvar, geom, crs='epsg:4326')
   return(my_sf)
+}
+
+#' Make Buffer
+#'
+#' @param sf_obj a simple feature dataframe
+#' @param dist (int) distance in meters to draw a buffer around
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' pts = make_point("my_site", 45, 45)
+#' pts_buff = make_buffer(pts, 5000)
+make_buffer <- function(sf_obj, dist=5000) {
+  coords_buffer = sf_obj %>%
+    sf::st_transform('epsg:3857') %>%
+    sf::st_buffer(dist) %>%
+    sf::st_transform('epsg:4326')
+  return(coords_buffer)
+}
+
+
+#' Extract Raster
+#'
+#' @param rast - raster object (use terra)
+#' @param clip_vec - vector (typically an sf data.frame)
+#' @param func_name - function to summarize extracted values (default=MEAN)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+extract_raster <- function(rast, clip_vec, func_name=mean) {
+  e = terra::extract(rast, clip_vec, fun=func_name, na.rm=T)
+  return(e)
 }
