9368cb8ede2d1495480627fe4e82da0b48e0b74a
barguzin <noi@ucsb.edu>
2023-07-18 13:01:41 -0600

    update ncdf_collector

--- R/ncdf_collector.R
+++ R/ncdf_collector.R
@@ -16,7 +16,8 @@
 #'  year_var=as.POSIXct('2020-01-01 14:45:18',
 #'  format="%Y-%m-%d %H:%M:%S",tz="UTC"),
 #'  site_id='KU', lon=-1.62, lat=6.7, dist=30000, var_name='soil_moisture')
-ncdf_collector <- function(ncdf_path, path_to_save, year=NULL, year_var=NULL, ...){
+ncdf_collector <- function(ncdf_path, path_to_save, year=NULL, year_var=NULL,
+                           ...){
 
   if (!missing(year_var)) {
     y = lubridate::year(year_var)
@@ -58,35 +59,37 @@ ncdf_collector <- function(ncdf_path, path_to_save, year=NULL, year_var=NULL, ..
   # check if cropped raster is empty
   if (sum(is.na(agg_bofr$sm))/length(agg_bofr$sm)==1) {
     print('Raster is empty')
-  }
-
-  # save clipped raster
-  if (missing(year)) {
-    print('No year supplied to function.')
-    vdir = paste0(path_to_save, d$site_id, '/', d$var_name, '/')
   } else {
-    print('non missing year')
-    vdir = paste0(path_to_save, d$site_id, '/', d$var_name, '/', year, '/')
+
+    # save clipped raster
+    if (missing(year)) {
+      print('No year supplied to function.')
+      vdir = paste0(path_to_save, d$site_id, '/', d$var_name, '/')
+    } else {
+      print('non missing year')
+      vdir = paste0(path_to_save, d$site_id, '/', d$var_name, '/', year, '/')
+    }
+
+    print(paste('creating directory in', vdir))
+    dir.create(vdir, recursive = T)
+
+    if (missing(year)) {
+      fdir = paste0(vdir, d$site_id, '_', d$var_name, '_', d$dist, 'm', '.tif')
+    } else {
+      fdir = paste0(vdir, d$site_id, '_', d$var_name, '_', d$dist, 'm', '_', year, '.tif')
+    }
+
+    print(paste('saving raster to', fdir))
+
+    # subset data by year
+    bofr_year = agg_bofr %>%
+      dplyr::filter(lubridate::year(time)==year) %>%
+      dplyr::select(sm) %>%
+      abind::adrop()
+
+    stars::write_stars(bofr_year, fdir, drive='GTiff')
+
   }
-
-  print(paste('creating directory in', vdir))
-  dir.create(vdir, recursive = T)
-
-  if (missing(year)) {
-    fdir = paste0(vdir, d$site_id, '_', d$var_name, '_', d$dist, 'm', '.tif')
-  } else {
-    fdir = paste0(vdir, d$site_id, '_', d$var_name, '_', d$dist, 'm', '_', year, '.tif')
-  }
-
-  print(paste('saving raster to', fdir))
-
-  # subset data by year
-  bofr_year = agg_bofr %>%
-    dplyr::filter(lubridate::year(time)==year) %>%
-    dplyr::select(sm) %>%
-    abind::adrop()
-
-  stars::write_stars(bofr_year, fdir, drive='GTiff')
 
   # prep yearly data as table
   e = mean(bofr_year$sm, na.rm=T)
