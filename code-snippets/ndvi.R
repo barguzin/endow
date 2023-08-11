d23fe7a42ef2f8add58398adaeb416477c3b0c77
barguzin <noi@ucsb.edu>
2023-08-10 20:49:55 -0700

    Update ndvi.R

--- code-snippets/ndvi.R
+++ code-snippets/ndvi.R
@@ -18,7 +18,15 @@ lon = 21.081161
 pt = endow::make_point(site_id = 'my_site', lon=lon, lat=lat)
 pt_buff = endow::make_buffer(pt)
 
+# Downloading the country boundary of Mongolia
+map_boundary <- geoboundaries("Mongolia")
+plot(st_geometry(map_boundary))
 
+# Defining filepath to save downloaded spatial file
+spatial_filepath <- "C:/Users/barguzin/Documents/VegetationData/mongolia.shp"
+
+# Saving downloaded spatial file on to our computer
+st_write(map_boundary, paste0(spatial_filepath))
 
 #tmap_mode('plot')
 tmap_mode('view')
@@ -29,4 +37,13 @@ tm_basemap(leaflet::providers$OpenStreetMap) +
   tm_polygons(alpha=.25, col = 'blue') +
   tm_shape(pt) +
   tm_dots(size=.5, col='blue')
+
+# create options file using gui
+
+# get data
+# --> Specify the path to a valid options file saved in advance from MODIStsp GUI
+opts_file <- "C:/Users/barguzin/Documents/VegetationData/out_params.json"
+
+# --> Launch the processing
+MODIStsp(gui = FALSE, opts_file = opts_file)
 
