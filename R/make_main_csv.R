01d004012febcb006e38222f8d1c5e0432f5b9e5
barguzin <noi@ucsb.edu>
2023-09-02 14:56:11 -0700

    Create make_main_csv.R

---
+++ R/make_main_csv.R
@@ -0,0 +1,15 @@
+#' make_main_csv
+#'
+#' Collect single csv for each variable, year and buffer into
+#' one big csv file for further processing.
+#'
+#' @param processed_dir (char) - dir with processed csv files
+#' @param output_dir (char) - output dir where main.csv will be saved
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples
+make_main_csv <- function(processed_dir, output_dir) {
+  return()
+}
